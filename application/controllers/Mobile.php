978f1657adde60623d41d436d8c1ca807a3a9813
duwilaja <apaaja.software@gmail.com>
2026-03-18 11:57:36 +0700

    pc/tc/tm

---
+++ application/controllers/Mobile.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mobile extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		// Your own constructor code
+	}
+	
+	public function index()
+	{
+		echo "";
+	}
+	public function privacy_policy()
+	{
+		$this->load->view('privacy-policy',[]);
+	}
+	public function term_condition()
+	{
+		$this->load->view('term-condition',[]);
+	}
+	public function tm_jogja($p)
+	{
+		$this->load->view('tm_'.$p,[]);
+	}
+}
